adc4aef8e59db978e765a66b9837fe62a69b8b95
stevebstorm <steve.lorent@gmail.Com>
2022-10-28 14:40:54 +0200

    Ajoutez des fichiers projet.

---
+++ DemoJWTIA/Mappers/Mappers.cs
@@ -0,0 +1,28 @@
+﻿using Demo_ASP_MVC_06_Session.Domain.Entities;
+using DemoJWTIA.Models;
+
+namespace DemoJWTIA.Mappers
+{
+    public static class Mappers
+    {
+        public static Member ToBLL(this AuthRegisterViewModel f)
+        {
+            return new Member
+            {
+                Email = f.Email,
+                Password = f.Password,
+                Pseudo = f.Pseudo
+            };
+        }
+
+        public static ConnectedUserDTO ToDTO(this Member member)
+        {
+            return new ConnectedUserDTO
+            {
+                Email = member.Email,
+                Pseudo = member.Pseudo,
+                MemberId = member.MemberId
+            };
+        }
+    }
+}
---
+++ DemoJWTIA/Tools/TokenManager.cs
@@ -0,0 +1,45 @@
+﻿using Demo_ASP_MVC_06_Session.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DemoJWTIA.Tools
+{
+    public class TokenManager
+    {
+        private string _secret;
+
+        public TokenManager(IConfiguration config)
+        {
+            _secret = config.GetSection("JwtInfo").GetSection("secret").Value;
+        }
+
+        public string GenerateToken(Member member)
+        {
+            //Créer la signing key
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+            SigningCredentials credential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+
+            //Création du payload /claims
+            Claim[] myClaims = new[]
+            {
+                new Claim("MemberId", member.MemberId.ToString()),
+                new Claim(ClaimTypes.GivenName, member.Pseudo)
+               
+            };
+
+            JwtSecurityToken securityToken = new JwtSecurityToken(
+                    claims: myClaims,
+                    signingCredentials: credential,
+                    expires : DateTime.Now.AddDays(1)
+                );
+
+            //Génération du token
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(securityToken);
+        }
+    }
+}
